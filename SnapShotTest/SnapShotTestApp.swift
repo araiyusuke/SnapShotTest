a249ff29cb24455af65440a6ca2d1648ff1e8792
araiyusuke <arai-t-ryousan06@tanita.co.jp>
2022-04-21 10:20:22 +0900

    Initial Commit

---
+++ SnapShotTest/SnapShotTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapShotTestApp.swift
+//  SnapShotTest
+//
+//  Created by 名前なし on 2022/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SnapShotTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
